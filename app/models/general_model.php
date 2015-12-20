54e9f27f8a8b7e005fb5b476193902f51efd82de
Muhammet DURMUŞ <magicslip@gmail.com>
2015-12-20 20:42:52 +0200

    Update general_model.php

--- app/models/general_model.php
+++ app/models/general_model.php
@@ -6,4 +6,9 @@ class General_model extends CI_Model {
     	$query = $this->db->order_by('date_added','DESC')->get('haberler', 3);
     	return $query->result_array();
 	}
+	public function get_top_shows()
+	{
+    	$query = $this->db->order_by('imdb_rating','DESC')->get('diziler', 10);
+    	return $query->result_array();
+	}
 }
