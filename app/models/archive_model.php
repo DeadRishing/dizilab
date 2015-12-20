51ef6e5fc0a71debb78b4bc640e297f087a93a7f
Muhammet DURMUŞ <magicslip@gmail.com>
2015-12-20 20:42:32 +0200

    Delete archive_model.php

--- app/models/archive_model.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Archive_model extends CI_Model {
-
-    public function get_top_shows()
-	{
-    	$query = $this->db->order_by('imdb_rating','DESC')->get('diziler', 10);
-    	return $query->result_array();
-	}
-}
